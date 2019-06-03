0b6aa2b077a4eec1bb4e526afefde67624868551
Serhiy Savchuk and Tamara Trefilova <serhiy@>
2019-06-03 16:30:25 -0700

    added some functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
+var sentence;
+var first;
+var last;
+var rest;
+
 $(function(){
-  $("h1").click(function(){
-    alert("This is a header.");
-  })
   $("p").click(function(){
-    alert("This is a paragraph.");
-  })
+    sentence = prompt("enter a sentence:");
+    first = sentence.charAt(0).toUpperCase();
+    last = sentence.charAt(sentence.length-1).toUpperCase();
+    rest = sentence.subString(0,sentence.length-1);
+  });
+
   $("img").click(function(){
-    alert("This is a picture.");
-  })
+    alert(first+last);
+
+    });
 });
